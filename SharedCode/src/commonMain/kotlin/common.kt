db9fb00ca634a7f6416ad522482a0ee7b625fa6b
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2019-11-01 16:13:58 -0400

    Doc comments: Do not survive to generated header :(

--- SharedCode/src/commonMain/kotlin/common.kt
+++ SharedCode/src/commonMain/kotlin/common.kt
@@ -1,6 +1,13 @@
 package jeremywsherman.com.handson.kmp
 
+/** The name of the currently running platform. */
 expect fun platformName(): String
 
+/**
+ Let's see if this doc comment makes it through to the module!
+
+ You'd have to either be generating a Swift module or embedding headers for that to work.
+ And I do see headers in the framework output, so! Let's see if this survives.
+ */
 fun createApplicationScreenMessage() =
     "Kotlin Rocks on ${platformName()}"
