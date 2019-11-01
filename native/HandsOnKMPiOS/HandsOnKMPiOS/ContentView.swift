1f0ec22471b329d4e4382465aaac8da7dce60eac
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2019-11-01 16:15:23 -0400

    Add button to test debugging experience

--- native/HandsOnKMPiOS/HandsOnKMPiOS/ContentView.swift
+++ native/HandsOnKMPiOS/HandsOnKMPiOS/ContentView.swift
@@ -11,7 +11,28 @@ import SharedCode
 
 struct ContentView: View {
     var body: some View {
-        Text(CommonKt.createApplicationScreenMessage())
+        VStack {
+            Text(CommonKt.createApplicationScreenMessage())
+            Button(action: {
+                NSLog("i will do the thing")
+                /*
+                 To actually break on this, you need to either:
+
+                 - Use regex-break: rb createApplicationScreenMessage
+                 - Use the wacky long underlying symbol name actually exposed at runtime:
+                   `kfun:jeremywsherman.com.handson.kmp.createApplicationScreenMessage()kotlin.String`
+
+                 The latter is the only way to set a persistent breakpoint using Xcode vs the LLDB console.
+                 The easy way to find that name is to regex-break then copy-paste.
+                 It does not seem to be in the generated Obj-C symbol table,
+                 though it is amongst in the unexported text symbols that you can dump using `nm`.
+                 */
+                let message = CommonKt.createApplicationScreenMessage()
+                NSLog("lo, i have done the thing, and the message is: \(message)")
+            }) {
+                Text(NSLocalizedString("Do The Thing", comment: "button title"))
+            }
+        }
     }
 }
 
